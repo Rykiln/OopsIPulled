bbfe32e8ad915fb90ee1712e9d056a37986121b9
Rykiln <41490808+Rykiln@users.noreply.github.com>
2021-08-15 01:09:33 -0500

    Update ready.js
    Removed excessive console logging and an unnecessary commented line.

--- events/ready.js
+++ events/ready.js
@@ -2,9 +2,7 @@ module.exports = {
   name: `ready`,
   once: true,
   execute(client) {
-    console.log(`ready`)
     console.log(`${client.user.username} Bot Is Now Online!`);
-    // console.log(`This bot is a Tier ${client.guilds.resolve(GuildID).premiumTier} server with ${client.guilds.resolve(GuildID).premiumSubscriptionCount} boosts!`); // Commented because GuildID is not being passed correctly from index.js
     console.log(`Twitch API Request interval is set to ${process.env.OOPS_TWITCH_REFRESH} seconds.`);
     console.log(Date());
     console.log('========================================');
