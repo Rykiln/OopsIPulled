a05853596f80e981838a1d11b235e6f72b4c332c
Rykiln <41490808+Rykiln@users.noreply.github.com>
2021-08-18 14:21:14 -0500

    Update index.js
    Destructured fs
    Added comments
    Grouped Global Collections
    Added Parameters for Event Handlers
    Updated Twitch Path

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ require('dotenv').config();
 const Discord = require('discord.js');
 const { Client, Collection, Intents } = require(`discord.js`);
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MEMBERS] })
+const {readdirSync} = require(`fs`);
 
-const fs = require('fs');
-
-const BotStatusLive = true;
+// Client Status Boolean: Live(True)/Test(False)
+const BotStatusLive = false;
 let Token;
 let GuildID;
 if (BotStatusLive) {
@@ -20,24 +20,26 @@ if (BotStatusLive) {
   Token = process.env.TEST_TOKEN;
   GuildID = process.env.TEST_GUILDID;
 }
+
+// Global Collections
+client.commands = new Collection();
 client.cooldowns = new Collection();
 
 // Event Handler: Listens For Defined Events Located In The Events Folder
-const eventFiles = fs.readdirSync(`./events`).filter(file => file.endsWith(`.js`));
+const eventFiles = readdirSync(`./events`).filter(file => file.endsWith(`.js`));
 for (const file of eventFiles) {
   const event = require(`./events/${file}`);
   if (event.once) {
-    client.once(event.name, (...args) => event.execute(...args, client, Discord, GuildID));
+    client.once(event.name, (...args) => event.execute(...args, client));
   } else {
-    client.on(event.name, (...args) => event.execute(...args, client, Discord, GuildID));
+    client.on(event.name, (...args) => event.execute(...args, client, Discord, GuildID, Token));
   }
 }
 
 // Command Handler: Listens For Messages With A Defined Command Prefix Located In The Commands Folder Recursively
-client.commands = new Collection();
-const commandFolders = fs.readdirSync('./commands');
+const commandFolders = readdirSync('./commands');
 commandFolders.forEach((folder) => {
-  const commandFiles = fs.readdirSync(`./commands/${folder}`).filter((file) => file.endsWith('.js'));
+  const commandFiles = readdirSync(`./commands/${folder}`).filter((file) => file.endsWith('.js'));
   commandFiles.forEach((file) => {
     const command = require(`./commands/${folder}/${file}`);
     client.commands.set(command.name, command);
@@ -46,4 +48,4 @@ commandFolders.forEach((folder) => {
 
 // Initialize Clients
 client.login(Token);
-require('./api/twitch')(client, GuildID);
+require('./Twitch/twitch')(client, GuildID);
